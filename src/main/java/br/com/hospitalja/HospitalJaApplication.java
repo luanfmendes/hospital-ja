59739781272215b6443c1059cb3eafc4d0e2811a
luanfmendes <lna_mendes@hotmail.com>
2023-03-06 10:09:39 -0300

    first commit

---
+++ src/main/java/br/com/hospitalja/HospitalJaApplication.java
@@ -0,0 +1,13 @@
+package br.com.hospitalja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hospitalja/HospitalJaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hospitalja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
